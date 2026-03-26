61c5a4b6e5ff0e1b4d19e3f875b7faef2d2a51bf
Shan Muhammad <shanisitedeveloper@gmail.com>
2026-03-26 12:09:29 +0500

    Update HomePage component to handle user roles and redirect accordingly
    Refactored the HomePage component to asynchronously fetch the current user and implement role-based redirection. Users with the "superadmin" role are redirected to the superadmin dashboard, while all other users are directed to the general dashboard. This change enhances user experience by providing appropriate access based on roles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import { redirect } from "next/navigation"
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "./(home)/dashboard/fetch";
 
-const HomePage = () => {
-  redirect("/dashboard")
-  return null
-}
+const HomePage = async () => {
+  const user = await getCurrentUser();
+  if (user?.role === "superadmin") {
+    redirect("/dashboard/superadmin");
+  }
+  redirect("/dashboard");
+};
 
-export default HomePage
+export default HomePage;
