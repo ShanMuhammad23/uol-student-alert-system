16f1f32baf11c78ee961b397f3802d571c5894e6
Shan Muhammad <shanisitedeveloper@gmail.com>
2026-04-22 15:15:10 +0500

    Update Attendance Classes Held Property to be Optional in Enrollment Types
    This commit modifies the `attendanceClassesHeld` property in the `DepartmentStats`, `ProgramStats`, and `InstructorStats` types to be optional. This change allows for greater flexibility in data handling, accommodating scenarios where attendance data may not be available.

--- src/lib/enrollment/types.ts
+++ src/lib/enrollment/types.ts
@@ -27,7 +27,7 @@ export type DepartmentStats = {
   yellowGpa: number;
   redGpa: number;
   yellowAttendance: number;
-  attendanceClassesHeld: number;
+  attendanceClassesHeld?: number;
   redAttendance: number;
   attendanceMissing?: number;
 };
@@ -40,7 +40,7 @@ export type ProgramStats = {
   redGpa: number;
   yellowAttendance: number;
   redAttendance: number;
-  attendanceClassesHeld: number;
+  attendanceClassesHeld?: number;
   attendanceMissing?: number;
 };
 
@@ -52,7 +52,7 @@ export type InstructorStats = {
   redGpa: number;
   yellowAttendance: number;
   redAttendance: number;
-  attendanceClassesHeld: number;
+  attendanceClassesHeld?: number;
   attendanceMissing?: number;
 };
 
